430a2d9641dd5bba7243e08accb86d63e03f85d5
Tayyab Haseeb <tayyabhaseeb5@gmail.com>
2022-07-06 00:54:10 +0500

    improvement

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <div className="nav--container">
+      <nav>
+        <img src="../public/images/world.png" width="40px" />
+      </nav>
+    </div>
+  );
+}
